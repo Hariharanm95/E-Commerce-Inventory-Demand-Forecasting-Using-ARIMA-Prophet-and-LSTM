6223f3178224cefabf116fcef7b71beef77db253
HARIHARAN M <harihm8838@gmail.com>
2025-02-24 22:59:00 +0530

    Cleaning project structure

--- backend/config/auth.js
+++
@@ -1,4 +0,0 @@
-// Configuration of JWT secret will be done in the .env file, this file is not needed
-// module.exports = {
-//     jwtSecret: process.env.JWT_SECRET || 'your-default-jwt-secret', // Fallback for dev
-// };
